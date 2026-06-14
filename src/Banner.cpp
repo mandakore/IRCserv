b40a1a0ff72a474561a33d760f8f87ae6761e598
mandakore <galaxyfish11@gmail.com>
2026-06-14 12:08:19 +0900

    cpp formatを無効化

--- src/Banner.cpp
+++ src/Banner.cpp
@@ -1,16 +1,3 @@
-// format使用厳禁！！！！！！！！！！！！！！！！！！！！！！！
-// format使用厳禁！！！！！！！！！！！！！！！！！！！！！！！
-// format使用厳禁！！！！！！！！！！！！！！！！！！！！！！！
-// format使用厳禁！！！！！！！！！！！！！！！！！！！！！！！
-// format使用厳禁！！！！！！！！！！！！！！！！！！！！！！！
-// format使用厳禁！！！！！！！！！！！！！！！！！！！！！！！
-// format使用厳禁！！！！！！！！！！！！！！！！！！！！！！！
-// format使用厳禁！！！！！！！！！！！！！！！！！！！！！！！
-// format使用厳禁！！！！！！！！！！！！！！！！！！！！！！！
-// format使用厳禁！！！！！！！！！！！！！！！！！！！！！！！
-// format使用厳禁！！！！！！！！！！！！！！！！！！！！！！！
-// format使用厳禁！！！！！！！！！！！！！！！！！！！！！！！
-
 #include <iostream>
 #include <unistd.h>
 #include "Banner.hpp"
@@ -31,6 +18,7 @@ void printFrame (const char *chars[][6], int count) {
 	}
 }
 
+// clang-format off
 void printBannerTypewriter () {
 	const char *F[6] = {"███████╗",
 						"██╔════╝",
@@ -73,13 +61,14 @@ void printBannerTypewriter () {
 						"██║     ",
 						"╚██████╗",
 						" ╚═════╝"};
-	const char *chars[6][6] = {{F[0], F[1], F[2], F[3], F[4], F[5]},
-							   {T[0], T[1], T[2], T[3], T[4], T[5]},
-							   {U[0], U[1], U[2], U[3], U[4], U[5]},
-							   {I[0], I[1], I[2], I[3], I[4], I[5]},
-							   {R[0], R[1], R[2], R[3], R[4], R[5]},
-							   {C[0], C[1], C[2], C[3], C[4], C[5]}};
-
+	const char *chars[6][6] = {
+		{F[0], F[1], F[2], F[3], F[4], F[5]},
+		{T[0], T[1], T[2], T[3], T[4], T[5]},
+		{U[0], U[1], U[2], U[3], U[4], U[5]},
+		{I[0], I[1], I[2], I[3], I[4], I[5]},
+		{R[0], R[1], R[2], R[3], R[4], R[5]},
+		{C[0], C[1], C[2], C[3], C[4], C[5]}};
+	// clang-format on
 	int frame;
 
 	frame = 1;
@@ -87,8 +76,7 @@ void printBannerTypewriter () {
 		std::cout << "\033[H\033[J";
 		printFrame (chars, frame);
 		// usleep (250000);
-		for (int i = 0; i < 5e8; ++i){
-			
+		for (int i = 0; i < 5e8; ++i) {
 		}
 		frame++;
 	}
