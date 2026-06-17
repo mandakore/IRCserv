2d7573595b26cc5e0d9af0818d48da2633504dd9
Sohei Yamazaki <snow.sohei@gmail.com>
2026-06-17 21:47:17 +0900

    bugfix(port): add definition of port

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 
 #define BANNER 0
+#define PORTMIN 6665
+#define PORTMAX 6669
 
 volatile sig_atomic_t g_server_running = 1;
 
@@ -14,7 +16,7 @@ void signalHandler (int signum) {
 
 int main (int argc, char **argv) {
 	if (argc != 3) {
-		std::cout << "./Usage: ./ircserv <port[0-65535]> <password>" << std::endl;
+		std::cout << "./Usage: ./ircserv <port[6665-6669]> <password>" << std::endl;
 		return 1;
 	}
 	signal (SIGINT, signalHandler);
@@ -22,7 +24,7 @@ int main (int argc, char **argv) {
 	try {
 		DEBUG ("main debug test");
 		long portInput = std::atol (argv[1]);
-		if (portInput < 0 || portInput > 65535) {
+		if (portInput < PORTMIN || portInput > PORTMAX) {
 			std::cout << "Error: Invalid Port [" << portInput << "] detected." << std::endl;
 			return 1;
 		}
